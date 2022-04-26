55a9faa0f2e3b91d8f5534f87e95e5b1fdf1da2a
Sanjeet Chaudhary <sanjeet_chaudhary77@protonmail.com>
2022-04-27 02:19:11 +0545

    coding file handling logic

--- login_portal.c
+++ login_portal.c
@@ -5,5 +5,42 @@
 bool login_portal()
 {
 	//trying file handling staff_credentials.csv first
+	
+	FILE *fp;
+	int i, j, k, count1, count2;
+	count1 = 0;
+	count2 = 0;
+	char ch, txt_stream1[12], txt_stream2[12]; //don't know if this needs to be an array
+					       //gonna cheat and do 11 char long string arrays (coz samples are that long)
+					       //will worry about dynamic memory allocations
+					       //--for varying length of uid and passwords later
+	
+	fp = fopen("./staff_credentials.csv", "r");
+	fp = fp + 21*(sizeof(char)); //the length of ' "user_id","password", ' is 21 characters
+				     //cheating again
+	
+	ch = getc(fp);
+	while(ch != EOF)
+	{
+		while((count1 != 2) && (ch != EOF))
+		{
+			if (ch == ",")
+			{
+				count1++;
+			}
+			if (strlen(txt_stream1) < 11)
+			{
+				strcat(txt_stream1, ch);
+			}
+			if ((strlen(txt_stream1) >= 11) && (strlen(txt_stream2) < 11))
+			{
+				strcat(txt_stream2, ch);
+			}
+		}
+		printf("user_id : %s , password : %s", txt_stream1, txt_stream2);
+	}
+
+
+
 	return 0;
 }
