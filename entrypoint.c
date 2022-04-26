ba8fd00009a9072e49fc0e94b42bab333b8e61d4
Sanjeet Chaudhary <sanjeet_chaudhary77@protonmail.com>
2022-04-27 00:48:43 +0545

    possible login_portal function return type

--- entrypoint.c
+++ entrypoint.c
@@ -5,8 +5,11 @@
 //extern  customer_page();
 int main()
 {
-    char    login_accessvar[], logout_var;
-    login_access    =   login_portal(); //returns tbd
+    //char    login_accessvar[]; // logout_var;
+    bool login_accessvar; // maybe I don't need to be fancy and use strings here
+			  // I could just have login_portal() return bool i.e. 2 values
+			  // though the 'true'/'false' implications is making me icky, let's see what happens
+    login_accessvar    =   login_portal(); //returns tbd
     switch (login_accessvar)
     {
     	case "Staff"/* constant-expression */:
