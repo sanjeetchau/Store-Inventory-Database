0fd8127ca2f71691d319adf9e453e7cc0fcebdbd
Sanjeet Chaudhary <sanjeet_chaudhary77@protonmail.com>
2022-05-03 01:36:05 +0545

    fixed logic error, 2 ifs ran simulta..

--- login_portal.c
+++ login_portal.c
@@ -52,38 +52,40 @@ int main (void)
 		count1 = 0;
 		txt_stream1[0] = '\0';
 		txt_stream2[0] = '\0';
-		a = true;
 		while( (count1 != 2) && (ch != EOF) ) //'a' is the bool var that checks when both txts are full
                                 //--> doesn't work even when the conditional is negated
                                 //--> i.e. while( !((count1 == 2) || !a) ) gives exact same logical error
 		{
 			//puts("\ninner while start");							//test print
 			//putchar(ch);									//test print
-			if (ch == ',')
+			j = 0;
+      if (ch == ',')
 			{
 				count1++;
 				ch = getc(fp);
+        //putchar(ch);
+        //printf("count %d\n", count1);
 				//puts("\ninner while first conditional");				//test print
-				a = a && (ch != EOF);
-        a = !a;
+				//a = a && (ch != EOF);
+        //a = !a;
         continue;
 			}
 			if (!(strlen(txt_stream1) == 11))
 			{
 				//puts("\ninner while second conditional");				//test print
 				strncat(txt_stream1, &ch, 1);
-				//puts("\n inner while second conditional after strcat");			//test print
+        j++;                                              //flag to check whether this condition branch has been executed
 				//txt_stream1 = txt_stream1.c_str();
 			}
-			if ( (strlen(txt_stream1) == 11) && !(strlen(txt_stream2) == 11) )		//try "==11" instead of >=
+			if ( !(j == 1) && !(strlen(txt_stream2) == 11) )		//try "==11" instead of >=
 			{
 				//puts("\ninner while third conditional");				//test print
 				strncat(txt_stream2, &ch, 1);
 			}
 			ch = getc(fp);
-			a = (strlen(txt_stream1) == 11) && (strlen(txt_stream2) == 11);
-			a = a && (ch != EOF);
-			a = !a;
+			//a = (strlen(txt_stream1) == 11) && (strlen(txt_stream2) == 11);
+			//a = a && (ch != EOF);
+			//a = !a;
 			//puts("\ninner while end");							//test print
 		}
 		printf("\nuser id : %s and password : %s ", txt_stream1, txt_stream2);
@@ -92,7 +94,7 @@ int main (void)
 		//puts(txt_stream1);
 		//puts(txt_stream2);
 	}
-	puts("\nend hello\n");										//test print; not working 
+	//puts("\nend hello\n");										//test print; not working 
 	
 
 	fclose(fp);
