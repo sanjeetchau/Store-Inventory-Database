7a215520704ef43bdae8f186573c94f97c0388ce
Sanjeet Chaudhary <sanjeet_chaudhary77@protonmail.com>
2022-05-11 00:17:09 +0545

    fixing header includes

--- entrypoint.c
+++ entrypoint.c
@@ -1,10 +1,11 @@
-//#include<stdio.h>
+#include<stdio.h>
 //#include<string.h>
   //extern  login_portal();                   //(old idea) prolly won't do this, prolly doesn't even work 
   //extern  staff_page();
   //extern  customer_page();
 void entrypoint()
 {
+  /*
     //char    login_accessvar[]; // logout_var;
     bool login_accessvar; // maybe I don't need to be fancy and use strings here
 			  // I could just have login_portal() return bool i.e. 2 values
@@ -12,7 +13,7 @@ void entrypoint()
     login_accessvar    =   login_portal(); //returns tbd
     switch (login_accessvar)
     {
-    	case "0"/* bool-value for "staff" */:
+    	case "0"// bool-value for "staff" //:
         	staff_page();
         	break;
     	case "1":
@@ -23,4 +24,6 @@ void entrypoint()
         	printf("\n Error.\n") //Or maybe the default should be to show the available items list (info only)
         	break;
     }
+  */
+  printf("entrypoint module\n");
 }
